bf084db97e143e583f21c0ad8a0b341559c9ce44
sanghyeog <sanghyeog_kim@tmax.co.kr>
2020-02-07 13:57:42 +0900

    feat($inputView): updated inputView and inputTest
    Added:
    - Main(class): get user input for number selecting manually
    - UserInputValidator(class): throw exceptions when wrong input for manual count and manual pick
    - UserInputTest(test): invalidInputForManualCountTest2 (excceed available manual count)
    Updated:
    - UserInputTest(test): check thrown messages

--- src/main/java/lotto/Main.java
+++ src/main/java/lotto/Main.java
@@ -3,7 +3,7 @@ package lotto;
 import java.util.Scanner;
 
 public class Main {
-
+    static final String DELIMITER = ",";
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
 
@@ -13,12 +13,24 @@ public class Main {
 
         System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
         int userInput4ManualCount = userInput.nextInt();
-        UserInputValidator.validateManualCount(userInput4ManualCount);
+        UserInputValidator.validateManualCount(userInput4Purchase, userInput4ManualCount);
 
         String userInput4ManualPick;
+        int[][] intArray4ManualPick = new int[userInput4ManualCount][];
         for ( int i = 0 ; i < userInput4ManualCount; i++) {
+            System.out.println("수동으로 구매할 번호를 입력해 주세요.");
             userInput4ManualPick = userInput.next();
-
+            intArray4ManualPick[i] = covertStringArrayToIntArray(userInput4ManualPick.split(DELIMITER));
+            UserInputValidator.validateManualPick(intArray4ManualPick[i]);
         }
+    }
+
+    private static int[] covertStringArrayToIntArray(String[] strArray){
+        int[] intArray = new int[strArray.length];
+        for( int i =0 ; i < strArray.length; i++) {
+            intArray[i] = Integer.parseInt(strArray[i]);
+        }
+
+        return intArray;
     }
 }
--- test/UserInputTest.java
+++ test/UserInputTest.java
@@ -3,45 +3,58 @@ import lotto.UserInputValidator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
 public class UserInputTest {
+
     @Test
     @Description("로또 구입 금액에 대한 유저 입력 값이 1000 미만이면 IllegalArgumentException 이 발생하는지 테스트")
-    public void invalidInputForPurchaseTest(){
-        int testInput = -1000;
+    public void invalidInputForPurchaseTest() {
+        int purchaseInput = -1000;
 
-        Assertions.assertThrows(IllegalArgumentException.class, ()-> {
-            UserInputValidator.validatePurchaseInput(testInput);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            UserInputValidator.validatePurchaseInput(purchaseInput);
         });
     }
 
     @Test
     @Description("로또 수동 구매에 대한 유저 입력 값이 0 미만이면 IllegalArgumentException 이 발생하는지 테스트")
-    public void invalidInputForManualCountTest(){
-        int testInput = -1000;
+    public void invalidInputForManualCountTest() {
+        int purchaseInput = 5000;
+        int manualCountInput = -1;
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
+            UserInputValidator.validateManualCount(purchaseInput, manualCountInput);
+        }).withMessage("수동 입력 개수는 자연수만 입력 가능합니다.");
+    }
 
-        Assertions.assertThrows(IllegalArgumentException.class, ()-> {
-            UserInputValidator.validateManualCount(testInput);
-        });
+    @Test
+    @Description("로또 수동 구매에 대한 유저 입력 값이 구매 가능한 개수(로또 구입 금액 / 1000)보다 IllegalArgumentException 이 발생하는지 테스트")
+    public void invalidInputForManualCountTest2() {
+        int purchaseInput = 5000;
+        int manualCountInput = 6;
+
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
+            UserInputValidator.validateManualCount(purchaseInput, manualCountInput);
+        }).withMessage("로또 구매 금액을 초과하였습니다.");
     }
 
     @Test
     @Description("로또 번호 수동 입력에 대한 유저 입력 값이 1~45 사이의 수가 아니면 IllegalArgumentException 이 발생하는지 테스트")
-    public void invalidInputForManualPickTest(){
-        int[] testInput = {1,2,3,4,5,48};
+    public void invalidInputForManualPickTest() {
+        int[] testInput = {1, 2, 3, 4, 5, 48};
 
-        Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
             UserInputValidator.validateManualPick(testInput);
-        });
+        }).withMessage("로또 번호는 1~45 사이의 수만 입력 가능합니다.");
     }
 
     @Test
     @Description("로또 번호 수동 입력에 대한 유저 입력 값이 6개가 아니면 아니면 IllegalArgumentException 이 발생하는지 테스트")
-    public void invalidInputForManualPickCountsTest(){
-        int[] testInput = {1,2,3,4,5,6,7,8};
+    public void invalidInputForManualPickCountsTest() {
+        int[] testInput = {1, 2, 3, 4, 5, 6, 7, 8};
 
-        Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
             UserInputValidator.validateManualPick(testInput);
-        });
+        }).withMessage("6개의 로또 번호를 선택하셔야 합니다.");
     }
-
 }
