a51b433a2a0fbcfebe4af7ab1092b2e5893deca8
sanghyeog <sanghyeog_kim@tmax.co.kr>
2020-02-07 14:06:37 +0900

    style($inputView): arrange indentation

--- src/main/java/lotto/Main.java
+++ src/main/java/lotto/Main.java
@@ -3,7 +3,9 @@ package lotto;
 import java.util.Scanner;
 
 public class Main {
+
     static final String DELIMITER = ",";
+
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
 
@@ -17,17 +19,18 @@ public class Main {
 
         String userInput4ManualPick;
         int[][] intArray4ManualPick = new int[userInput4ManualCount][];
-        for ( int i = 0 ; i < userInput4ManualCount; i++) {
+        for (int i = 0; i < userInput4ManualCount; i++) {
             System.out.println("수동으로 구매할 번호를 입력해 주세요.");
             userInput4ManualPick = userInput.next();
-            intArray4ManualPick[i] = covertStringArrayToIntArray(userInput4ManualPick.split(DELIMITER));
+            intArray4ManualPick[i] = covertStringArrayToIntArray(
+                userInput4ManualPick.split(DELIMITER));
             UserInputValidator.validateManualPick(intArray4ManualPick[i]);
         }
     }
 
-    private static int[] covertStringArrayToIntArray(String[] strArray){
+    private static int[] covertStringArrayToIntArray(String[] strArray) {
         int[] intArray = new int[strArray.length];
-        for( int i =0 ; i < strArray.length; i++) {
+        for (int i = 0; i < strArray.length; i++) {
             intArray[i] = Integer.parseInt(strArray[i]);
         }
 
--- src/main/java/lotto/UserInputValidator.java
+++ src/main/java/lotto/UserInputValidator.java
@@ -1,29 +1,31 @@
 package lotto;
 
 public class UserInputValidator {
+
     final static int LOTTO_PRICE = 1000;
+
     public static void validatePurchaseInput(int purchaseInput) {
-        if ( purchaseInput < LOTTO_PRICE) {
+        if (purchaseInput < LOTTO_PRICE) {
             throw new IllegalArgumentException("1000원 이상 구매하셔야 합니다.");
         }
     }
 
     public static void validateManualCount(int purchase, int manualCountInput) {
-        if ( manualCountInput < 0 ) {
+        if (manualCountInput < 0) {
             throw new IllegalArgumentException("수동 입력 개수는 자연수만 입력 가능합니다.");
         }
 
-        if ( manualCountInput >purchase / LOTTO_PRICE) {
+        if (manualCountInput > purchase / LOTTO_PRICE) {
             throw new IllegalArgumentException("로또 구매 금액을 초과하였습니다.");
         }
     }
 
     public static void validateManualPick(int[] manualPickInput) {
-        if ( manualPickInput.length != 6) {
+        if (manualPickInput.length != 6) {
             throw new IllegalArgumentException("6개의 로또 번호를 선택하셔야 합니다.");
         }
-        for ( int pickNumber : manualPickInput) {
-            if( pickNumber < 0 || pickNumber > 45) {
+        for (int pickNumber : manualPickInput) {
+            if (pickNumber < 0 || pickNumber > 45) {
                 throw new IllegalArgumentException("로또 번호는 1~45 사이의 수만 입력 가능합니다.");
             }
         }
