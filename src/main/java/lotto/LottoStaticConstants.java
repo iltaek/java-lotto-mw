6eb78b4c7820eab4461cfd591c9d2a96c0355724
skyrainyday <lcc87@hanmail.net>
2020-02-11 17:20:35 +0900

    feat(model) : Add class - LottoNumber, LottoTicket, LottoStaticConstants.

---
+++ src/main/java/lotto/LottoStaticConstants.java
@@ -0,0 +1,7 @@
+package lotto;
+
+public class LottoStaticConstants {
+    public static final int LOTTO_TICKET_NUMBER_SIZE = 6;
+
+    public static final String LOTTO_TICKET_NUMBER_SIZE_ERROR_MESSAGE = "로또 번호는 서로 다른 숫자로 정확히 6개가 입력되어야 합니다.";
+}
---
+++ src/main/java/lotto/LottoTicket.java
@@ -0,0 +1,22 @@
+package lotto;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class LottoTicket {
+    private final List<LottoNumber> lottoTicket;
+
+    public LottoTicket(List<LottoNumber> lottoNumbers) {
+        if (lottoNumbers == null || lottoNumbers.size() != LottoStaticConstants.LOTTO_TICKET_NUMBER_SIZE) {
+            throw new IllegalArgumentException(LottoStaticConstants.LOTTO_TICKET_NUMBER_SIZE_ERROR_MESSAGE);
+        }
+
+        Set<LottoNumber> lottoNumbersWithoutDuplicates = new HashSet<>(lottoNumbers);
+        if (lottoNumbersWithoutDuplicates.size() != LottoStaticConstants.LOTTO_TICKET_NUMBER_SIZE) {
+            throw new IllegalArgumentException(LottoStaticConstants.LOTTO_TICKET_NUMBER_SIZE_ERROR_MESSAGE);
+        }
+
+        this.lottoTicket = lottoNumbers;
+    }
+}
